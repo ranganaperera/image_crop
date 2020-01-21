9a4372ac578c7f209a780ed646475d2300f3547f
Nilusha De Silva <nilisha.7desilva@gmail.com>
2020-01-21 19:46:00 +0530

    update image ratio change to 0.85

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -244,7 +244,7 @@ class CropState extends State<Crop> with TickerProviderStateMixin, Drag {
       setState(() {
         _image = imageInfo.image;
         _scale = imageInfo.scale;
-        _ratio = 0.8;
+        _ratio = 0.85;
 
         final viewWidth = _boundaries.width / (_image.width * _scale * _ratio);
         final viewHeight =
