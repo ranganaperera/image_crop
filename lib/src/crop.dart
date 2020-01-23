8d4cccca30febb535beaa04cdcd967cff09482ea
Malshan Perera <malshan.perera@itelasoft.com>
2020-01-23 09:16:25 +0530

    width fixed

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -252,12 +252,11 @@ class CropState extends State<Crop> with TickerProviderStateMixin, Drag {
       setState(() {
         _image = imageInfo.image;
         _scale = imageInfo.scale;
-        _ratio = 0.87;
+        _ratio = 0.85;
         print("$_ratio");
 
         final viewWidth = _boundaries.width / (_image.width * _scale * _ratio);
-        final viewHeight =
-            _boundaries.height / (_image.height * _scale * _ratio);
+        final viewHeight = _boundaries.height / (_image.height * _scale * _ratio);
         _area = _calculateDefaultArea(
           viewWidth: viewWidth,
           viewHeight: viewHeight,
