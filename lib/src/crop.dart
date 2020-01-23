0da951007d49221abe7e430878866f2058849d3c
Nilusha De Silva <nilisha.7desilva@gmail.com>
2020-01-23 13:09:10 +0530

    change image height 9.0

--- lib/src/crop.dart
+++ lib/src/crop.dart
@@ -252,7 +252,7 @@ class CropState extends State<Crop> with TickerProviderStateMixin, Drag {
       setState(() {
         _image = imageInfo.image;
         _scale = imageInfo.scale;
-        _ratio = 0.87;
+        _ratio = 0.9;
         print("$_ratio");
 
         final viewWidth = _boundaries.width / (_image.width * _scale * _ratio);
@@ -261,7 +261,7 @@ class CropState extends State<Crop> with TickerProviderStateMixin, Drag {
           viewWidth: viewWidth,
           viewHeight: viewHeight,
           imageWidth: _image.width,
-          imageHeight: _image.height+30,
+          imageHeight: _image.height,
         );
         _view = Rect.fromLTWH(
           (viewWidth - 1.0) / 2,
